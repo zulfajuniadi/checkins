decd627efd438af8af12f3b14ae6906822568835
Zulfa Juniadi <zulfajuniadi@gmail.com>
2016-04-08 20:03:53 +0800

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,95 @@
+angular.module('app.controllers', ['nl2br', 'uuid'])
+
+.run(function($rootScope, Session){
+
+    $rootScope.Session = Session;
+
+})
+  
+.controller('myCheckinsCtrl', function($scope, $timeout) {
+    $scope.doRefresh = function() {
+        $timeout(function(){
+           $scope.$broadcast('scroll.refreshComplete');
+        }, 1000);
+    }
+
+    $scope.removeCheckin = function(checkin) {
+        $scope.Session.checkins.splice($scope.Session.checkins.indexOf(checkin), 1);
+    }
+})
+   
+.controller('checkinCtrl', function(
+    $scope,
+    $ionicLoading,
+    $timeout,
+    $cordovaGeolocation,
+    $ionicPlatform,
+    $http,
+    reverseGeocoder,
+    $state,
+    uuid4
+) {
+    $scope.latitude = '';
+    $scope.longitude = '';
+    $scope.address = '';
+    $scope.description = '';
+    $scope.map = '';
+
+    $ionicLoading.show({
+        template: 'Getting your location...'
+    });
+    $ionicPlatform.ready(function() {
+        $cordovaGeolocation.getCurrentPosition({
+            timeout: 20000,
+            enableHighAccuracy: false
+        }).then(function(position){
+            $scope.latitude = position.coords.latitude.toFixed(5);
+            $scope.longitude = position.coords.longitude.toFixed(5);
+            reverseGeocoder
+                .getAddress($scope.latitude, $scope.longitude)
+                .then(function(address){
+                    $scope.address = address.text;
+                    $scope.map = address.map;
+                    $ionicLoading.hide();
+                }, function(){
+                    $ionicLoading.show({
+                        template: 'Error getting address from location.'
+                    });
+                    $timeout(function(){
+                        $ionicLoading.hide();
+                    }, 1000);
+                });
+        }, function(error){
+            $ionicLoading.show({
+                template: 'Error getting your location.'
+            });
+            $timeout(function(){
+                $ionicLoading.hide();
+            }, 1000);
+
+        });
+    });
+
+    $scope.save = function() {
+        console.log($scope);
+        $scope.Session.checkins.unshift({
+            id: uuid4.generate(),
+            latitude: $scope.latitude,
+            longitude: $scope.longitude,
+            address: $scope.address,
+            description: $scope.description,
+            map: $scope.map,
+        })
+        $state.go('myCheckins');
+    }
+})
+   
+.controller('locationCtrl', function($scope, $stateParams) {
+    $scope.checkin = $scope.Session.checkins.reduce(function(carry, checkin){
+        if(checkin.id == $stateParams.id)
+            carry = checkin;
+        return carry;
+    }, {});
+    console.log($scope.checkin, $stateParams)
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,35 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+  .state('myCheckins', {
+    url: '/checkins',
+    templateUrl: 'templates/myCheckins.html',
+    controller: 'myCheckinsCtrl'
+  })
+
+  .state('checkin', {
+    url: '/checkin',
+    templateUrl: 'templates/checkin.html',
+    controller: 'checkinCtrl'
+  })
+
+  .state('location', {
+    url: '/location/:id',
+    templateUrl: 'templates/location.html',
+    controller: 'locationCtrl'
+  })
+
+$urlRouterProvider.otherwise('/checkins')
+
+  
+
+});
